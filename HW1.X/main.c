6a5bd2f4a791b05e9e7a4c391e2ad68780fe1b68
mariosebasco <mariosebasco@yahoo.com>
2017-03-29 20:08:41 -0500

    added some stuff

--- HW1.X/main.c
+++ HW1.X/main.c
@@ -1,40 +1,39 @@
 #include<xc.h>           // processor SFR definitions
 #include<sys/attribs.h>  // __ISR macro
 
-// DEVCFG0
-#pragma config DEBUG = 01 // no debugging
-#pragma config JTAGEN = 0 // no jtag
-#pragma config ICESEL = 11 // use PGED1 and PGEC1
-#pragma config PWP = 111111111 // no write protect
-#pragma config BWP = 0 // no boot write protect
-#pragma config CP = 0 // no code protect
+#pragma config DEBUG = OFF // no debugging
+#pragma config JTAGEN = OFF // no jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // no write protect
+#pragma config BWP = OFF // no boot write protect
+#pragma config CP = OFF // no code protect
 
 // DEVCFG1
-#pragma config FNOSC = 011 // use primary oscillator with pll
-#pragma config FSOSCEN = 0 // turn off secondary oscillator
-#pragma config IESO = 0 // no switching clocks
-#pragma config POSCMOD = 10 // high speed crystal mode
-#pragma config OSCIOFNC = 1 // free up secondary osc pins *********************
-#pragma config FPBDIV = 00 // divide CPU freq by 1 for peripheral bus clock
-#pragma config FCKSM = 10 // do not enable clock switch
-#pragma config WDTPS = 10100 // slowest wdt
-#pragma config WINDIS = 1 // no wdt window
-#pragma config FWDTEN = 0 // wdt off by default
-#pragma config FWDTWINSZ = 11 // wdt window at 25%
+#pragma config FNOSC = PRIPLL // use primary oscillator with pll
+#pragma config FSOSCEN = OFF // turn off secondary oscillator
+#pragma config IESO = OFF // no switching clocks
+#pragma config POSCMOD = HS // high speed crystal mode
+#pragma config OSCIOFNC = OFF // free up secondary osc pins
+#pragma config FPBDIV = DIV_1 // divide CPU freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // do not enable clock switch
+#pragma config WDTPS = PS1 // slowest wdt
+#pragma config WINDIS = OFF // no wdt window
+#pragma config FWDTEN = OFF // wdt off by default
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
 
 // DEVCFG2 - get the CPU clock to 48MHz
-#pragma config FPLLIDIV = 001 // divide input clock to be in range 4-5MHz
-#pragma config FPLLMUL = 111 // multiply clock after FPLLIDIV
-#pragma config FPLLODIV = 001 // divide clock after FPLLMUL to get 48MHz
-#pragma config UPLLIDIV = 001 // divider for the 8MHz input clock, then multiply by 12 to get 48MHz for USB
-#pragma config UPLLEN = 0 // USB clock on
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
+#pragma config UPLLIDIV = DIV_2 // divider for the 8MHz input clock, then multiply by 12 to get 48MHz for USB
+#pragma config UPLLEN = ON // USB clock on
 
 // DEVCFG3
-#pragma config USERID = 0 // some 16bit userid, doesn't matter what
-#pragma config PMDL1WAY = 0 // allow multiple reconfigurations
-#pragma config IOL1WAY = 0 // allow multiple reconfigurations
-#pragma config FUSBIDIO = 1 // USB pins controlled by USB module
-#pragma config FVBUSONIO = 1 // USB BUSON controlled by USB module
+#pragma config USERID = 0 // some 16bit userubmit the link to your repid, doesn't matter what
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+#pragma config FUSBIDIO = ON // USB pins controlled by USB module
+#pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
 
 
 int main() {
@@ -56,9 +55,23 @@ int main() {
     // do your TRIS and LAT commands here
 
     __builtin_enable_interrupts();
+    
+    TRISAbits.TRISA4 = 0; //LED output pin
+    TRISBbits.TRISB4 = 1; //Button input pin
+    LATAbits.LATA4 = 1; //turn on LED
+            
 
     while(1) {
 	    // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
 		  // remember the core timer runs at half the CPU speed
+         _CP0_SET_COUNT(0);
+        while(_CP0_GET_COUNT() < 12000) //12,000 in order to get 1KHz
+        {
+            while(!PORTBbits.RB4)//button is pressed?
+            {
+                LATAbits.LATA4 = 0; //Turn off LED
+            }
+        }
+        LATAINV = 0b10000; //if off turn on and vice versa
     }
 }
